b931c514ae2b0b3c9e00ee3877d40085a13461cd
王雪松 <xswang@outlook.com>
2016-06-21 15:30:34 +0800

    TwoSum

---
+++ src/com/goribun/leetcode/TwoSum.java
@@ -0,0 +1,20 @@
+package com.goribun.leetcode;
+
+/**
+ * Created by wangxuesong on 16/6/21.
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution.
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        return nums;
+    }
+}
