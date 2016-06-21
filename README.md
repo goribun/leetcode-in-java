1c0138859992e0c75edfa63dd04ae73348c5660b
王雪松 <xswang@outlook.com>
2016-06-21 16:39:32 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcode-in-java
+
+- It may not be the best answer, just a personal exercise.
--- src/com/goribun/leetcode/TwoSum.java
+++ src/com/goribun/leetcode/TwoSum.java
@@ -11,10 +11,26 @@ public class TwoSum {
 
     public static void main(String[] args) {
 
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(result[0] + "  " + result[1]);
     }
 
     public static int[] twoSum(int[] nums, int target) {
 
-        return nums;
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
     }
 }
